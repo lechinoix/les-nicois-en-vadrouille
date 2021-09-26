aeffa28486dbd5bc11ca04b046251598f5184f5e
lechinoix <nicolas.ngomai@gmail.com>
2021-09-26 23:35:51 +0200

    fix: create slider onMount

--- src/lib/components/slider.svelte
+++ src/lib/components/slider.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
 	import lightGallery from 'lightgallery';
 	import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.umd';
 	import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.umd';
 	import { formatAssetUrl } from '$lib/services/adventureService';
 	import type { Picture } from '$lib/types';
 
+  export const ssr = false;
 	export let pictures: Picture[] = [];
 	export let galleryName: string = 'lightgallery';
 
-	lightGallery(document.getElementById('lightgallery'), {
-		plugins: [lgZoom, lgThumbnail],
-		speed: 500
-	});
+  onMount(() => {
+    lightGallery(document.getElementById('lightgallery'), {
+      plugins: [lgZoom, lgThumbnail],
+      speed: 500
+    });
+  });
 </script>
 
 <div id={galleryName}>
 	{#each pictures as picture}
-		<a data-lg-size={`${picture.width}-${picture.height}`}>
-			<img alt={picture.alternativeText} src={formatAssetUrl(picture.url)} />
+		<a data-lg-size={`${picture.width}-${picture.height}`} data-src="{formatAssetUrl(picture.formats.large.url)}">
+			<img alt={picture.alternativeText} src={formatAssetUrl(picture.formats.small.url)} class="image__inpage" />
 		</a>
 	{/each}
 </div>
@@ -26,4 +30,8 @@
 	@import 'lightgallery/css/lightgallery.css';
 	@import 'lightgallery/css/lg-zoom.css';
 	@import 'lightgallery/css/lg-thumbnail.css';
+
+  .image__inpage {
+    max-width: 400px;
+  }
 </style>
