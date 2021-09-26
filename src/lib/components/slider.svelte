8e77cfb5f7efc63de5b7e82c58cce581766f21bb
lechinoix <nicolas.ngomai@gmail.com>
2021-09-26 23:56:31 +0200

    fix: do not build gallery on server

--- src/lib/components/slider.svelte
+++ src/lib/components/slider.svelte
@@ -1,27 +1,36 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+	import { browser } from '$app/env';
+	import { onMount } from 'svelte';
 	import lightGallery from 'lightgallery';
 	import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.umd';
 	import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.umd';
 	import { formatAssetUrl } from '$lib/services/adventureService';
 	import type { Picture } from '$lib/types';
 
-  export const ssr = false;
+	export const ssr = false;
 	export let pictures: Picture[] = [];
 	export let galleryName: string = 'lightgallery';
 
-  onMount(() => {
-    lightGallery(document.getElementById('lightgallery'), {
-      plugins: [lgZoom, lgThumbnail],
-      speed: 500
-    });
-  });
+	onMount(() => {
+		if (!browser) return;
+		lightGallery(document.getElementById('lightgallery'), {
+			plugins: [lgZoom, lgThumbnail],
+			speed: 500
+		});
+	});
 </script>
 
 <div id={galleryName}>
 	{#each pictures as picture}
-		<a data-lg-size={`${picture.width}-${picture.height}`} data-src="{formatAssetUrl(picture.formats.large.url)}">
-			<img alt={picture.alternativeText} src={formatAssetUrl(picture.formats.small.url)} class="image__inpage" />
+		<a
+			data-lg-size={`${picture.width}-${picture.height}`}
+			data-src={formatAssetUrl(picture.formats.large.url)}
+		>
+			<img
+				alt={picture.alternativeText}
+				src={formatAssetUrl(picture.formats.small.url)}
+				class="image__inpage"
+			/>
 		</a>
 	{/each}
 </div>
@@ -31,7 +40,7 @@
 	@import 'lightgallery/css/lg-zoom.css';
 	@import 'lightgallery/css/lg-thumbnail.css';
 
-  .image__inpage {
-    max-width: 400px;
-  }
+	.image__inpage {
+		max-width: 400px;
+	}
 </style>
