5a1233330d2ae2f9b9c8bed147c5208b651c2b4d
lechinoix <nicolas.ngomai@gmail.com>
2021-09-27 00:51:56 +0200

    feat: add filtered pages

--- src/lib/services/adventureService.ts
+++ src/lib/services/adventureService.ts
@@ -1,8 +1,25 @@
 import config from '$lib/config';
+import { AdventureStatus } from '$lib/constants';
 import type { Adventure } from '$lib/types';
 
-export const getAdventures = async (): Promise<Adventure[]> => {
-	const res = await fetch(`${config.BASE_API_URL}/adventures?_sort=date:DESC`);
+export const getAdventuresDone = async (): Promise<Adventure[]> => {
+	const res = await fetch(
+		`${config.BASE_API_URL}/adventures?status=${AdventureStatus.DONE}&_sort=date:DESC`
+	);
+	const adventures = await res.json();
+	return adventures;
+};
+
+export const getAdventuresPlanned = async (): Promise<Adventure[]> => {
+	const res = await fetch(
+		`${config.BASE_API_URL}/adventures?status=${AdventureStatus.PLANNED}&_sort=date:DESC`
+	);
+	const adventures = await res.json();
+	return adventures;
+};
+
+export const getAdventuresTodo = async (): Promise<Adventure[]> => {
+	const res = await fetch(`${config.BASE_API_URL}/adventures?status=${AdventureStatus.TODO}`);
 	const adventures = await res.json();
 	return adventures;
 };
---
+++ src/routes/adventures/todo.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { getAdventuresTodo } from '$lib/services/adventureService';
+	import type { Adventure } from '$lib/types';
+	import TopoLink from '$lib/components/topoLink.svelte';
+
+	let adventures: Adventure[] = [];
+	let error = null;
+
+	onMount(async () => {
+		try {
+			adventures = await getAdventuresTodo();
+		} catch (e) {
+			error = e;
+		}
+	});
+</script>
+
+{#if error !== null}
+	{error}
+{:else}
+	<div class="container">
+		<h1>Our Little Adventures</h1>
+		<div class="row">
+			{#each adventures as adventure}
+				<div class="column one-half">
+					<a href={`/adventures/${adventure.id}`}>
+						<h3>{adventure.title}</h3>
+						<p>
+							<b>Date</b> : {adventure.date} <br />
+							<b>Sports</b> : {adventure.sports.reduce((acc, el) => `${acc} ${el.name}`, '')} <br />
+							<b>Topo</b> : {#each adventure.topo as topo}
+								<TopoLink {topo} />
+								<br />
+							{/each}
+						</p>
+					</a>
+				</div>
+			{/each}
+		</div>
+	</div>
+{/if}
