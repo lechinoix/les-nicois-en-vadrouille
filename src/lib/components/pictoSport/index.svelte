2af60b56456c681662b405dd47cf6e2eef0fcec9
lechinoix <nicolas.ngomai@gmail.com>
2021-10-31 23:35:56 +0100

    feat: add picto sports

---
+++ src/lib/components/pictoSport/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Sports } from '$lib/constants';
+	import HikingIcon from './icons/hiking.svelte';
+	import MountaineeringIcon from './icons/mountaineering.svelte';
+	import ResortSkiingIcon from './icons/resortSkiing.svelte';
+	import RockClimbingIcon from './icons/rockClimbing.svelte';
+	import SkiTouringIcon from './icons/skiTouring.svelte';
+
+	const sportToPicto = {
+		[Sports.HIKING]: HikingIcon,
+		[Sports.ALPINISM]: MountaineeringIcon,
+		[Sports.SPELEO]: MountaineeringIcon,
+		[Sports.SKI_ALPIN]: ResortSkiingIcon,
+		[Sports.ESCALADE_COUENNE]: RockClimbingIcon,
+		[Sports.ESCALADE_GRANDE_VOIE]: RockClimbingIcon,
+		[Sports.SKI_DE_RANDO]: SkiTouringIcon
+	};
+
+	export let sport;
+
+	let picto = sportToPicto[sport];
+</script>
+
+<div>
+	<svelte:component this={picto} />
+</div>
