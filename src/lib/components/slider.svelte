82966540ee6ebddbce9715f8659da4661e709210
lechinoix <nicolas.ngomai@gmail.com>
2021-11-09 00:09:06 +0100

    fix: fix bug with broken image

--- src/lib/components/slider.svelte
+++ src/lib/components/slider.svelte
@@ -28,17 +28,19 @@
 	class="cursor-pointer w-full h-40 overflow-x-scroll overflow-y-hidden whitespace-nowrap"
 >
 	{#each pictures as picture}
-		<a
-			class="inline-block mr-2 h-full"
-			data-lg-size={`${picture.width}-${picture.height}`}
-			data-src={formatAssetUrl(picture.url)}
-		>
-			<img
-				alt={picture.alternativeText}
-				src={formatAssetUrl(picture.formats.small.url)}
-				class="w-full h-full object-contain"
-			/>
-		</a>
+		{#if !!picture.url}
+			<a
+				class="inline-block mr-2 h-full"
+				data-lg-size={`${picture.width}-${picture.height}`}
+				data-src={formatAssetUrl(picture.url)}
+			>
+				<img
+					alt={picture.alternativeText}
+					src={formatAssetUrl(picture.formats.small.url)}
+					class="w-full h-full object-contain"
+				/>
+			</a>
+		{/if}
 	{/each}
 </div>
 
