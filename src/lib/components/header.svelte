60aa85bd3990bd4522a96f4efdd7f8df78c35734
lechinoix <nicolas.ngomai@gmail.com>
2021-11-09 23:31:44 +0100

    feat: add hover bg for dropdown

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -25,7 +25,7 @@
 			{#each sports as sport}
 				<a
 					href={`/sport/${sport.slug}`}
-					class="flex text-gray-700 block px-4 py-2 text-sm justify-start items-center"
+					class="flex text-gray-700 block px-4 py-2 text-sm justify-start items-center hover:bg-gray-100"
 					role="menuitem"
 					tabindex="-1"
 				>
