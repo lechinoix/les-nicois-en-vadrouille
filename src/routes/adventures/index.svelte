897e00bec5774a84cf8192a21d8206831359f5c9
lechinoix <nicolas.ngomai@gmail.com>
2021-12-02 23:01:39 +0100

    feat: use load function in adventures page

--- src/routes/adventures/index.svelte
+++ src/routes/adventures/index.svelte
@@ -1,21 +1,28 @@
+<script context="module">
+	export const prerender = true;
+	export async function load() {
+		const adventures = await getAdventuresDone();
+		return {
+			props: { adventures },
+			maxage: 86400
+		};
+	}
+</script>
+
 <script lang="ts">
+	import type { Adventure } from '$lib/types';
 	import { getAdventuresDone } from '$lib/services/adventureService';
 	import { truncateText } from '$lib/utils/string';
 	import AdventureCard from '$lib/components/adventureCard.svelte';
 	import Description from '$lib/components/description.svelte';
-	import Suspense from '$lib/components/suspense.svelte';
 
-	let adventuresPromise = getAdventuresDone();
+	export let adventures: Adventure[];
 </script>
 
-<Suspense contentPromise={adventuresPromise}>
-	<div slot="content" let:content={adventures}>
-		{#each adventures as adventure}
-			<AdventureCard {adventure}>
-				<Description className="pt-7">
-					{adventure.short_description || truncateText(adventure.description)}
-				</Description>
-			</AdventureCard>
-		{/each}
-	</div>
-</Suspense>
+{#each adventures as adventure}
+	<AdventureCard {adventure}>
+		<Description className="pt-7">
+			{adventure.short_description || truncateText(adventure.description)}
+		</Description>
+	</AdventureCard>
+{/each}
