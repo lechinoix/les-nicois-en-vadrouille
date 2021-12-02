0f865c1ded6db3588f2345d99f8a4ac4d7918913
lechinoix <nicolas.ngomai@gmail.com>
2021-12-02 23:03:07 +0100

    feat: use load function on adventure details page

--- src/routes/adventures/[id].svelte
+++ src/routes/adventures/[id].svelte
@@ -1,36 +1,42 @@
+<script context="module">
+	export const prerender = true;
+	export async function load({ page, fetch }) {
+		const adventure = await getAdventureById(page.params.id);
+		return {
+			props: { adventure },
+			maxage: 86400
+		};
+	}
+</script>
+
 <script lang="ts">
+	import type { Adventure } from '$lib/types';
 	import marked from 'marked';
 	import Slider from '$lib/components/slider.svelte';
 	import TopoLink from '$lib/components/topoLink.svelte';
 	import AdventureCard from '$lib/components/adventureCard.svelte';
-	import { page } from '$app/stores';
 	import { getAdventureById } from '$lib/services/adventureService';
 	import Description from '$lib/components/description.svelte';
 	import Container from '$lib/components/container.svelte';
-	import Suspense from '$lib/components/suspense.svelte';
 
-	let adventurePromise = getAdventureById($page.params.id);
+	export let adventure: Adventure;
 </script>
 
-<Suspense contentPromise={adventurePromise}>
-	<div slot="content" let:content={adventure}>
-		<AdventureCard {adventure} />
-		<Container>
-			<Description>{@html marked(adventure.description)}</Description>
+<AdventureCard {adventure} />
+<Container>
+	<Description>{@html marked(adventure.description)}</Description>
+	<br />
+	<br />
+	{#if adventure.topo && adventure.topo.length > 0}
+		<b>Topo</b> :
+		{#each adventure.topo as topo}
+			<TopoLink {topo} />
 			<br />
-			<br />
-			{#if adventure.topo && adventure.topo.length > 0}
-				<b>Topo</b> :
-				{#each adventure.topo as topo}
-					<TopoLink {topo} />
-					<br />
-				{/each}
-			{/if}
-			{#if adventure.pictures.length > 0}
-				<div class="mt-5">
-					<Slider pictures={adventure.pictures} />
-				</div>
-			{/if}
-		</Container>
-	</div>
-</Suspense>
+		{/each}
+	{/if}
+	{#if adventure.pictures.length > 0}
+		<div class="mt-5">
+			<Slider pictures={adventure.pictures} />
+		</div>
+	{/if}
+</Container>
