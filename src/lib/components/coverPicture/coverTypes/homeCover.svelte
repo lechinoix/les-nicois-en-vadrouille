d6c74a1768eef78b2ff677abcfd1a350ca2522b7
lechinoix <nicolas.ngomai@gmail.com>
2021-12-11 21:59:57 +0100

    fix: fix homepage cover size

--- src/lib/components/coverPicture/coverTypes/homeCover.svelte
+++ src/lib/components/coverPicture/coverTypes/homeCover.svelte
@@ -9,7 +9,7 @@
 		picture.formats.xlarge ? picture.formats.xlarge.url : picture.url;
 </script>
 
-<div class="relative w-screen flex bg-gray-400 h-112">
+<div class="relative w-screen flex bg-gray-400 h-1/3 max-h-120">
 	<div class="absolute w-full h-full flex justify-end pb-8 pr-10 bg-gray-900 bg-opacity-10">
 		<strong class="block text-white text-3xl text-right font-title font-light self-end">
 			Bienvenue sur le blog d'aventures
@@ -25,7 +25,7 @@
 </div>
 
 <style>
-	.h-112 {
-		height: 28rem;
+	.max-h-120 {
+		max-height: 30rem;
 	}
 </style>
