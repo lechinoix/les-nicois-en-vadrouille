1cf7db21a78333ac9d0991d2b2b9f830bd087017
lechinoix <nicolas.ngomai@gmail.com>
2021-12-15 00:13:11 +0100

    feat: display loader if page is too long to load

--- src/lib/components/suspense.svelte
+++ src/lib/components/suspense.svelte
@@ -5,14 +5,15 @@
 	const DISPLAY_LOADER_KEY = 'DISPLAY_LOADER';
 
 	export let fadeDuration = 400;
-	export let waitBeforeLoader = 400;
+	export let waitBeforeLoader = 500;
 	export let showLoaderDuration = 1000;
 	export let contentPromise;
 
 	let pageVisible = true;
 	let showLoader = false;
+	let pageContentPromise;
 
-	let pageContentPromise = Promise.race([
+	$: pageContentPromise = Promise.race([
 		new Promise((resolve) => setTimeout(() => resolve(DISPLAY_LOADER_KEY), waitBeforeLoader)),
 		contentPromise
 	]).then((result) => {
@@ -34,7 +35,7 @@
 		<div
 			transition:fade={{ duration: fadeDuration }}
 			on:introstart={() => (pageVisible = false)}
-			on:outroend={() => (pageVisible = true)}
+			on:outroend={() => ((showLoader = false), (pageVisible = true))}
 			class="w-screen h-screen flex justify-center items-center"
 		>
 			<slot name="loader">
@@ -43,7 +44,9 @@
 		</div>
 	{/if}
 {:then content}
-	{#if pageVisible}
+	{#if !showLoader}
+		<slot name="content" {content} />
+	{:else if pageVisible}
 		<div in:fade={{ duration: fadeDuration }}>
 			<slot name="content" {content} />
 		</div>
