df800770e29581980c6c70bb5019481e0929353b
lechinoix <nicolas.ngomai@gmail.com>
2021-12-24 18:04:55 +0100

    fix: fix server side generation

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -1,5 +1,5 @@
 export const getUrlWithNewSlug = (location: Location, slug: string): string => {
-	const urlSlugMatcher = /^(.*)_[a-z-]*/;
+	const urlSlugMatcher = /^(.*)_[a-z-#]*/;
 	const urlWithoutSlug = new RegExp(urlSlugMatcher).exec(location.href)[1];
 
 	return `${urlWithoutSlug}_${slug}`;
