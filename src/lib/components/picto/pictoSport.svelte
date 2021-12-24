058ece2512294be9359016112af42d888a21007d
lechinoix <nicolas.ngomai@gmail.com>
2021-12-24 16:55:21 +0100

    feat: use new sport pictos

--- src/lib/components/picto/pictoSport.svelte
+++ src/lib/components/picto/pictoSport.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
 	import { Sports } from '$lib/constants';
-	import HikingIcon from '../icons/sport/old/hiking.svelte';
-	import MountaineeringIcon from '../icons/sport/old/mountaineering.svelte';
-	import ResortSkiingIcon from '../icons/sport/old/resortSkiing.svelte';
-	import RockClimbingIcon from '../icons/sport/old/rockClimbing.svelte';
-	import SkiTouringIcon from '../icons/sport/old/skiTouring.svelte';
+	import HikeBoot from '../icons/sport/new/hikeBoot.svelte';
+	import RockClimbing from '../icons/sport/new/rockClimbing.svelte';
+	import SkiBoot from '../icons/sport/new/skiBoot.svelte';
+
+	const DEFAULT_ICON = HikeBoot;
 
 	const sportToPicto = {
-		[Sports.HIKING]: HikingIcon,
-		[Sports.ALPINISM]: MountaineeringIcon,
-		[Sports.SPELEO]: MountaineeringIcon,
-		[Sports.SKI_ALPIN]: ResortSkiingIcon,
-		[Sports.ESCALADE_COUENNE]: RockClimbingIcon,
-		[Sports.ESCALADE_GRANDE_VOIE]: RockClimbingIcon,
-		[Sports.SKI_DE_RANDO]: SkiTouringIcon
+		[Sports.HIKING]: HikeBoot,
+		[Sports.ALPINISM]: RockClimbing,
+		[Sports.SPELEO]: RockClimbing,
+		[Sports.SKI_ALPIN]: SkiBoot,
+		[Sports.ESCALADE_COUENNE]: RockClimbing,
+		[Sports.ESCALADE_GRANDE_VOIE]: RockClimbing,
+		[Sports.SKI_DE_RANDO]: SkiBoot
 	};
 
 	export let sport;
 	export let fill;
 
-	let picto = sportToPicto[sport];
+	let picto = sportToPicto.hasOwnProperty(sport) ? sportToPicto[sport] : DEFAULT_ICON;
 </script>
 
 <svelte:component this={picto} {fill} />
