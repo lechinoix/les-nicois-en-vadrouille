154d00d0129c6e1ca5a1f7701c0d6efd8302ab6e
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-06-25 15:35:38 +0200

    ✨ Clean old versions

--- src/lib/services/s3/backend.ts
+++ src/lib/services/s3/backend.ts
@@ -1,6 +1,6 @@
 import {
-	// DeleteObjectCommand,
-	// ListObjectsCommand,
+	DeleteObjectCommand,
+	ListObjectsCommand,
 	PutObjectCommand,
 	S3Client
 } from '@aws-sdk/client-s3';
@@ -17,11 +17,11 @@ const client = new S3Client({
 	}
 });
 
-// const maxVersionAgeInDays = 10;
-// const minVersionCount = 10;
+const maxVersionAgeInDays = 10;
+const minVersionCount = 15;
 
 export const uploadVersionedFile = async (folderName: ADVENTURE_TYPE, rawContent: string) => {
-	// await cleanOldVersions(folderName);
+	await cleanOldVersions(folderName);
 	await uploadFile(`${dataFolder}/${folderName}/${headFileName}`, rawContent);
 	await uploadFile(
 		`${dataFolder}/${folderName}/${dayjs().format('YYYYDDMM_HHmmss')}.json`,
@@ -29,29 +29,29 @@ export const uploadVersionedFile = async (folderName: ADVENTURE_TYPE, rawContent
 	);
 };
 
-// const cleanOldVersions = async (folderName: string) => {
-// 	const currentVersions = await client.send(
-// 		new ListObjectsCommand({
-// 			Bucket: bucketName,
-// 			Prefix: `${dataFolder}/${folderName}`
-// 		})
-// 	);
+const cleanOldVersions = async (folderName: string) => {
+	const currentVersions = await client.send(
+		new ListObjectsCommand({
+			Bucket: bucketName,
+			Prefix: `${dataFolder}/${folderName}`
+		})
+	);
 
-// 	const versionsToClean =
-// 		currentVersions.Contents?.sort(
-// 			(versionA, versionB) =>
-// 				dayjs(versionB.LastModified).unix() - dayjs(versionA.LastModified).unix()
-// 		)
-// 			?.slice(minVersionCount)
-// 			?.filter((version) =>
-// 				dayjs(version.LastModified).isBefore(dayjs().add(-maxVersionAgeInDays, 'day'))
-// 			) ?? [];
+	const versionsToClean =
+		currentVersions.Contents?.sort(
+			(versionA, versionB) =>
+				dayjs(versionB.LastModified).unix() - dayjs(versionA.LastModified).unix()
+		)
+			?.slice(minVersionCount)
+			?.filter((version) =>
+				dayjs(version.LastModified).isBefore(dayjs().add(-maxVersionAgeInDays, 'day'))
+			) ?? [];
 
-// 	for (const versionToClean of versionsToClean) {
-// 		if (!versionToClean.Key) continue;
-// 		await deleteFile(versionToClean.Key);
-// 	}
-// };
+	for (const versionToClean of versionsToClean) {
+		if (!versionToClean.Key) continue;
+		await deleteFile(versionToClean.Key);
+	}
+};
 
 const uploadFile = async (fileName: string, content: string) =>
 	client.send(
@@ -63,10 +63,10 @@ const uploadFile = async (fileName: string, content: string) =>
 		})
 	);
 
-// const deleteFile = async (fullPath: string) =>
-// 	client.send(
-// 		new DeleteObjectCommand({
-// 			Bucket: bucketName,
-// 			Key: fullPath
-// 		})
-// 	);
+const deleteFile = async (fullPath: string) =>
+	client.send(
+		new DeleteObjectCommand({
+			Bucket: bucketName,
+			Key: fullPath
+		})
+	);
